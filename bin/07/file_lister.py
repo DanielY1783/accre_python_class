9ece3b6b09085c99c60aa4b6ea8a888f21f63b8c
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-07-25 15:45:04 -0500

    Create dictionary of file sizes

--- bin/07/file_lister.py
+++ bin/07/file_lister.py
@@ -2,7 +2,7 @@
 # Email: daniel.yan@vanderbilt.edu
 # Date: 2018-07-25
 #
-# Description: My solution to the below exercise posed by Eric Appelt
+# Description: My solution to the below exercise posed by Eric Appelt:
 #
 # Exercise: Write a module with a public function that takes a directory path
 # as an argument and returns a dictionary all (regular) file names and their
@@ -24,7 +24,28 @@
 # Libraries
 import os
 
+def dict_file_sizes(directory):
+    """
+    Creates and returns a dictionary containing keys as files in a directory
+    and values as the sizes in the directory.
+    :param directory: String of directory to look in. Exception will be
+    raised if this is invalid.
+    :return: Dictionary containing keys as files in directory and values as
+    the sizes in the directory.
+    """
+    # Get list of files
+    files_list = os.listdir(directory)
 
+    # Get sizes of files and put in dictionary with key as file
+    files_dict = {}
+    for file in files_list:
+        dir_and_file = os.path.join(directory, file)
+        # Get valid files only
+        if os.path.isfile(dir_and_file):
+            files_dict[file] = os.path.getsize(dir_and_file)
+    # Return dictionary with file names as keys and sizes as values.
+    return files_dict
 
 if __name__ == '__main__':
     file_sizes_dict = dict_file_sizes(os.getcwd())
+    print(file_sizes_dict)
